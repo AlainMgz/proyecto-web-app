4fcbc31bce00712ac2044fed9c5c7bdaf9179a58
Samuel <samcarri@ucm.es>
2024-04-04 12:47:25 +0200

    Acceso a la pagina de reviews

--- includes/vistas/comun/cabecera.php
+++ includes/vistas/comun/cabecera.php
@@ -19,7 +19,7 @@ function showGreeting() {
 <header class="cabecera">
     <img src="<?= RUTA_IMGS ?>/logo.png" alt="Logo">
     <a href="<?= RUTA_APP ?>/index.php" class="menu_link">Estrenos</a>
-    <a href="#" class="menu_link">Reviews</a>
+    <a href="<?= RUTA_APP ?>/lastReviews.php" class="menu_link">Reviews</a>
     <a href="#" class="menu_link">Blog</a>
     <a href="#" class="menu_link">Rankings</a>
     <a href="#" class="menu_link" id="searchBtn">Buscar</a>
