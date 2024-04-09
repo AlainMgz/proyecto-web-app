cb1a225b957c8629efe8ff66f5f3b1790eea5ed9
pabjeb <125867273+pabjeb@users.noreply.github.com>
2024-04-09 15:47:57 +0200

    alinear login cabecera

--- includes/vistas/comun/cabecera.php
+++ includes/vistas/comun/cabecera.php
@@ -52,58 +52,69 @@
     function showGreeting()
     {
         if (!isset ($_SESSION["login"]) || $_SESSION["login"] === false) {
-            echo '<span>
-            Unknown user. <a href="login.php" class="menu_link">Login</a>              </span>';
+            echo '<li class="nav-item"><a href="login.php" class="nav-link">Unknown user. Login</a></li>';
         } else {
             $username = unserialize($_SESSION['user_obj'])->getNombreUsuario();
-            echo '<span class="dropdown">
-                  <a class="dropbtn" href="#">Hello ' . $username . '</a>
-                  <div class="dropdown-content">
-                    <a href="criticasUsuario.php">Tus críticas</a>
-                  </div>
-              </span>
-              <a href="includes/logout.php" class="menu_link">Logout</a>';
+            echo '
+                <ul class="navbar-nav mr-auto ml-auto">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Hello ' . $username . '
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="criticasUsuario.php">Tus críticas</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a href="includes/logout.php" class="nav-link">Logout</a>
+                    </li>
+                </ul>';
         }
         return;
     }
     ?>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.php">
-        <img src="<?= RUTA_IMGS ?>/logo.png" alt="Logo" class="img-fluid" style="max-height: 40px;">
-    </a>        
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php">
+            <img src="<?= RUTA_IMGS ?>/logo.png" alt="Logo" class="img-fluid" style="max-height: 40px;">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <!-- Lista de opciones del menú -->
-            <li class="nav-item">
-                <a href="<?= RUTA_APP ?>/index.php" class="nav-link">Estrenos</a>
-            </li>
-            <li class="nav-item">
-                <a href="<?= RUTA_APP ?>/lastReviews.php" class="nav-link">Reviews</a>
-            </li>
-            <li class="nav-item">
-                <a href="<?= RUTA_APP ?>/blog.php" class="nav-link">Blog</a>
-            </li>
-            <li class="nav-item">
-                <a href="<?= RUTA_APP ?>/ranking.php" class="nav-link">Ranking</a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link" id="searchBtn">Buscar</a>
-            </li>
-            <?php if (isset($_SESSION["user_obj"]) && unserialize($_SESSION["user_obj"])->getRole() == 1): ?>
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <!-- Lista de opciones del menú -->
                 <li class="nav-item">
-                    <a href="<?= RUTA_APP ?>/funcionalidades/agregarPelicula.php" class="nav-link">Añadir estreno</a>
+                    <a href="<?= RUTA_APP ?>/index.php" class="nav-link">Estrenos</a>
                 </li>
-            <?php endif; ?>
-        </ul>
-    </div>
-    <div class="login-text"><?php showGreeting(); ?></div>
-</nav>
-    
+                <li class="nav-item">
+                    <a href="<?= RUTA_APP ?>/lastReviews.php" class="nav-link">Reviews</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?= RUTA_APP ?>/blog.php" class="nav-link">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?= RUTA_APP ?>/ranking.php" class="nav-link">Ranking</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" id="searchBtn">Buscar</a>
+                </li>
+                <?php if (isset ($_SESSION["user_obj"]) && unserialize($_SESSION["user_obj"])->getRole() == 1): ?>
+                    <li class="nav-item">
+                        <a href="<?= RUTA_APP ?>/funcionalidades/agregarPelicula.php" class="nav-link">Añadir estreno</a>
+                    </li>
+                <?php endif; ?>
+            </ul>
+
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <?php showGreeting(); ?>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
 </body>
 
 </html>
