10f0864c441fde6116189083b7cf9c798a3047b5
Samuel <samcarri@ucm.es>
2024-04-10 17:58:49 +0200

    Estaba mal puesto el nombre de la pagina xd

--- Blog.php
+++ Blog.php
@@ -4,7 +4,7 @@ require_once RAIZ_APP . '/session_start.php';
 require_once __DIR__ . '/includes/DTOs/UsuarioDTO.php';
 require_once __DIR__ . '/includes/SAs/PeliculaSA.php';
 
-$contenidoPrincipal='<h1>Ranking de películas</h1>
+$contenidoPrincipal='<h1>Blog</h1>
 <p>Estamos trabajando para ofrecer esta funcionalidad lo antes posible, disculpe por las molestias<p/>
 ';
 
